232f50d8f718a9cf7899a7f5964328630bcc5121
Alexander Markin <markina@CT.local>
2021-07-23 16:03:00 +0200

    fundamentals

---
+++ 01_Fundamentals_Part_1/script.js
@@ -0,0 +1,42 @@
+/*let js = 'amazing';
+console.log(40 + 10 + 23 - 10);
+
+let firstName = 'Alex';
+console.log(firstName);
+
+let country = 'Germany';
+let continent = 'Europa';
+let population = '83000000';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(true);
+
+console.log(typeof true);
+console.log(typeof 'string');
+console.log(typeof 12);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+*/
+
+// can be changed
+let age = 30;
+age = 31;
+
+// cann't be changed
+const bithday = 1990;
+
+// not use(old)
+var job = 'programmer';
+job = 'cleaner'
+
+const language = 'Geramn';
+const continent = 'Europe';
+let population = '85000000';
