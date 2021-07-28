cbebf93e13ec78d332f87bba9b168ab36f76d12d
Alexander Markin <markina@CT.local>
2021-07-28 12:15:35 +0200

    did stuff

--- 01_Fundamentals_Part_1/script.js
+++ 01_Fundamentals_Part_1/script.js
@@ -26,17 +26,96 @@ console.log(typeof country);
 console.log(typeof language);
 */
 
-// can be changed
-let age = 30;
-age = 31;
+// // can be changed
+// let age = 30;
+// age = 31;
 
-// cann't be changed
-const bithday = 1990;
+// // cann't be changed
+// const bithday = 1990;
 
-// not use(old)
-var job = 'programmer';
-job = 'cleaner'
+// // not use(old)
+// var job = 'programmer';
+// job = 'cleaner'
 
-const language = 'Geramn';
-const continent = 'Europe';
-let population = '85000000';
+// const language = 'Geramn';
+// const continent = 'Europe';
+// let population = '85000000';
+
+
+// console.log(3 ** 3);
+
+// console.log('jonas' + ' ' + 'smith');
+
+// const marksHeight = 1.81;
+// const johnsHeight = 1.76;
+// const marksWeight = 85;
+// const johnsWeight = 88;
+
+// const marksBmi = marksWeight / marksHeight ** 2;
+// console.log(marksBmi);
+
+// const johnsBmi = johnsWeight / johnsHeight ** 2;
+// console.log(johnsBmi);
+
+// const markHigherBMI = marksBmi > johnsBmi;
+// console.log(markHigherBMI);
+
+
+// const firstName = 'Alex';
+// const job = 'entwickler';
+// const birtYear = '1987';
+// const year = '2021';
+
+// const alex = "I'm " + firstName + ", a " +
+//     (year - birtYear) + " years old " + job;
+// console.log(alex);
+
+
+// const alexNew = `I'm ${firstName}, 
+// a ${year - birtYear} years old ${job}`;
+// console.log(alexNew);
+
+// const age = 90;
+
+// if (age >= 18) {
+//     console.log(`You can have a driving license`)
+// } else {
+//     console.log(`You can have a driving license after ${18 - age} years`)
+
+// }
+
+// const year = 1991;
+// let century;
+// if (year > 1900) {
+//     century = 20;
+// }
+
+// const marksHeight = 1.81;
+// const johnsHeight = 1.76;
+// const marksWeight = 85;
+// const johnsWeight = 88;
+
+// const marksBmi = marksWeight / marksHeight ** 2;
+
+// const johnsBmi = johnsWeight / johnsHeight ** 2;
+
+// if (marksBmi > johnsBmi) {
+//     console.log(`Mark's BMI(${marksBmi}) is higher than John's(${johnsBmi})!`);
+// } else {
+//     console.log(`John's BMI(${johnsBmi}) is higher than Mark's!(${marksBmi})`)
+// }
+
+// //type conversion
+// const inputYear = '1991';
+// console.log(inputYear + 19);
+// console.log(Number(inputYear) + 19);
+
+// console.log(String(23));
+
+// //type coercion
+// console.log('I am ' + 23 + ' years old');
+
+// console.log('9 + 5')
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(`i'm`);
