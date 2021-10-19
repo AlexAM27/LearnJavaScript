7a584127b2db348388604716e7e3dc8e79661449
Alexander Markin <markina@CT.local>
2021-10-19 16:26:03 +0200

    game

--- 05_guess_my_number/script.js
+++ 05_guess_my_number/script.js
@@ -1,25 +1,42 @@
 'use strict';
 
 let guessNumber = Math.floor(Math.random() * 20 + 1);
-console.log(guessNumber);
 let userNumber;
+let score = 20;
+let bestScore = 0;
 
-function equalNumbers(someNumber) {
-    document.querySelector('.check').addEventListener('click', function() {
-        userNumber = Number(document.querySelector('input').value);
-        if(userNumber < someNumber) {
-            document.querySelector('.message').textContent = 'Too low!';
-        }
-        if(userNumber > someNumber) {
-            document.querySelector('.message').textContent = 'Too high!';
-        }
-        if(userNumber === someNumber) {
-            document.querySelector('.message').textContent = 'Correct number!';
-            document.querySelector('.show-number').textContent = userNumber;
-            document.querySelector('body').style.backgroundColor = 'green';
-        }
-    });
-};
+document.querySelector('.check').addEventListener('click', function() {
+    userNumber = Number(document.querySelector('input').value);
+    if(userNumber < guessNumber) {
+        document.querySelector('.message').textContent = 'Too low!';
+        score--;
+        document.querySelector('.score').textContent = `${score}`;
+    }
+    if(userNumber > guessNumber) {
+        document.querySelector('.message').textContent = 'Too high!';
+        score--;
+        document.querySelector('.score').textContent = `${score}`;
+    }
+    if(userNumber === guessNumber) {
+        document.querySelector('.message').textContent = 'Correct number!';
+        document.querySelector('.show-number').textContent = userNumber;
+        document.querySelector('body').style.backgroundColor = '#7AEC0D';
+        score--;
+        document.querySelector('.score').textContent = `${score}`;
+        bestScore = score > bestScore ? score : bestScore;
+        console.log(bestScore);
+        document.querySelector('.highscore').textContent = `${bestScore}`;
+    }
+});
 
-equalNumbers(guessNumber);
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    document.querySelector('.score').textContent = `${score}`;
+    document.querySelector('.show-number').textContent = '?';
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('body').style.backgroundColor = 'black';
+    document.querySelector('input').value = '';
+    guessNumber = Math.floor(Math.random() * 20 + 1);
+});
+
 
