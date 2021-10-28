21ad50732c6655f031a5cfd1d08f89935123230f
AlexAM27 <alexandermarkin27@gmail.com>
2021-10-28 21:27:25 +0200

    overlay

--- 06_modal_window/script.js
+++ 06_modal_window/script.js
@@ -1,2 +1,27 @@
 'use strict';
 
+const windowButton1 = document.querySelector('.left_button');
+
+const windowButton2 = document.querySelector('.middle_button');
+
+const windowButton3 = document.querySelector('.right_button');
+
+const modalWindow = document.querySelector('.hidden');
+
+const closeModalWindowButton = document.querySelector('img');
+
+windowButton1.addEventListener('click', function() {
+    modalWindow.style.display = 'block';
+});
+
+windowButton2.addEventListener('click', function() {
+    modalWindow.style.display = 'block';
+});
+
+windowButton3.addEventListener('click', function() {
+    modalWindow.style.display = 'block';
+});
+
+closeModalWindowButton.addEventListener('click', function() {
+    modalWindow.style.display = 'none';
+});
